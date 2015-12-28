4234d54da618722e2b0ec5058b0d047ae75945ab
redknot <redknotmiaoyuqiao@gmail.com>
2015-12-28 16:35:02 +0800

    first

---
+++ app/src/main/java/com/dynamsoft/tessocr/TessOCR.java
@@ -0,0 +1,54 @@
+package com.dynamsoft.tessocr;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.util.Log;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+public class TessOCR {
+	private TessBaseAPI mTess;
+	
+	public TessOCR() {
+		// TODO Auto-generated constructor stub
+		mTess = new TessBaseAPI();
+		String datapath = Environment.getExternalStorageDirectory() + "/tesseract/";
+
+		String language = "eng";
+		File dir = new File(datapath + "tessdata/");
+		if (!dir.exists()) 
+			dir.mkdirs();
+		mTess.init(datapath, language);
+	}
+
+	private byte[] InputStreamToByte(InputStream is) throws IOException {
+		ByteArrayOutputStream bytestream = new ByteArrayOutputStream();
+		int ch;
+		while ((ch = is.read()) != -1) {
+			bytestream.write(ch);
+		}
+		byte imgdata[] = bytestream.toByteArray();
+		bytestream.close();
+		return imgdata;
+	}
+	
+	public String getOCRResult(Bitmap bitmap) {
+		
+		mTess.setImage(bitmap);
+		String result = mTess.getUTF8Text();
+
+		return result;
+    }
+	
+	public void onDestroy() {
+		if (mTess != null)
+			mTess.end();
+	}
+	
+}
